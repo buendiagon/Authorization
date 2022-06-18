36d7a6f4ad8c7b89aef3789edda853bd04c2d496
Daniel Adrian Gonzalez Buendia <rsi.dev26@uis.edu.co>
2022-06-18 17:26:16 -0500

    fix(pom): fixing pom

--- src/main/java/com/uis/authorization/dto/SignInDTO.java
+++ src/main/java/com/uis/authorization/dto/SignInDTO.java
@@ -3,7 +3,6 @@ package com.uis.authorization.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.Serial;
 import java.io.Serializable;
 
 /**
@@ -12,7 +11,6 @@ import java.io.Serializable;
 @Getter
 @Setter
 public class SignInDTO implements Serializable {
-    @Serial
     private static final long serialVersionUID = 3796908352572727437L;
 
     private String username;
